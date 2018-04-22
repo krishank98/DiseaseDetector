fca42a79016a483f6ddea975e8ce401ae65d6819
krishank98 <krishankumarr98@gmail.com>
2018-04-22 11:36:16 +0530

    creating activities

---
+++ app/src/main/java/com/example/krish/diseasedetector/StartLogin.java
@@ -0,0 +1,19 @@
+package com.example.krish.diseasedetector;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by krish on 22-04-2018.
+ */
+
+public class StartLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
+
